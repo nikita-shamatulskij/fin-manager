6ae0ac43604a7980a6e9534bece8b1d7696ef3ff
Nikita Shamatulskij <165020639+nikita-shamatulskij@users.noreply.github.com>
2026-07-29 18:51:57 +0300

    Initial commit: base infrastructure and datasource config

---
+++ src/main/java/com/solutions/apex/fin_manager/FinManagerApplication.java
@@ -0,0 +1,13 @@
+package com.solutions.apex.fin_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solutions/apex/fin_manager/FinManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutions.apex.fin_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
