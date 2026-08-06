1ed9aa776b4318ddd67b8e9ea1e9932b0fdebfe1
Nikita Shamatulskij <165020639+nikita-shamatulskij@users.noreply.github.com>
2026-08-06 23:49:31 +0300

    fix: add explicit user password mapping

--- src/main/java/com/solutions/apex/fin_manager/mapper/UserMapper.java
+++ src/main/java/com/solutions/apex/fin_manager/mapper/UserMapper.java
@@ -4,9 +4,12 @@ import com.solutions.apex.fin_manager.dto.UserRegistrationRequestDTO;
 import com.solutions.apex.fin_manager.dto.UserResponseDTO;
 import com.solutions.apex.fin_manager.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "passwordHash", source = "password")
     User toEntity(UserRegistrationRequestDTO dto);
+
     UserResponseDTO toDTO(User user);
 }
